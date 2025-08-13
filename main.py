354bd0c816fa2ed3f4b8d52322c71592b38a06d8
diabolus_est_machina <maximilianrichter@posteo.de>
2025-08-13 19:19:37 +0200

    finished the bookbot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import sys
 from stats import get_book_text, count_words, count_chars, sort_char_dict
 
 def main():
-    filepath = "books/frankenstein.txt"
+    filepath = sys.argv[1]
     file_content = get_book_text(filepath)
     word_count = count_words(file_content)
     char_count = count_chars(file_content)
@@ -13,5 +14,8 @@ def main():
 
     
 
-
-main()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    main()
